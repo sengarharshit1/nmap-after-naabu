128889448fedf7452bf3a769d507d7de7ccd8281
Harshit Sengar <36125236+sengarharshit1@users.noreply.github.com>
2020-09-07 14:41:22 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+echo "
+                                                                            
+,   .                       ,---.|                 ,   .          |         
+|\  |,-.-.,---.,---.   ,---.|__. |--- ,---.,---.   |\  |,---.,---.|---..   .
+| \ || | |,---||   |---,---||    |    |---'|    ---| \ |,---|,---||   ||   |
+`  `'` ' '`---^|---'   `---^`    `---'`---'`       `  `'`---^`---^`---'`---'
+               |                                                            
+"
+echo "
+creator : @sengarharshit1
+https://www.twitter.com/sengarharshit1
+https://www.linkedin.com/in/sengarharshit1
+"
+
 naabu -ports full -hL $1 -o naabu_scan.txt
 cat scan.txt | cut -d ':' -f2 | sort -u > ports.txt
 for i in `cat scan.txt | cut -d ':' -f1 |uniq`; do nmap -sS -Pn -sV -T3 -p $(sed ':a;N;$!ba;s/\n/,/g' ports.txt) $i -oN $i.txt;done
